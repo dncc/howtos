b95635c6bc84de641eb4332129337e3606ae20b3
Dragan Cvetinovic <dcvetinovic@gmail.com>
2012-07-01 10:49:02 +0200

    how to set cyrilic layout in arch

--- ruby.rb
+++ ruby.rb
@@ -14,21 +14,21 @@ ruby -ne '
 sort -rn
 
 #-------------------- Same thing in AWK ----------------------------
-curl -s http://www.gutenberg.org/files/1080/1080.txt |
-awk '
-    BEGIN { FS="[^a-zA-Z]+" }
-
-    {
-        for (i=1; i<=NF; i++) {
-            word = tolower($i)
-            words[word]++
-        }
-    }
-
-    END {
-        for (w in words)
-             printf("%3d %s\n", words[w], w)
-    }
-' |
-sort -rn
+#curl -s http://www.gutenberg.org/files/1080/1080.txt |
+#awk '
+#    BEGIN { FS="[^a-zA-Z]+" }
+#
+#    {
+#        for (i=1; i<=NF; i++) {
+#            word = tolower($i)
+#            words[word]++
+#        }
+#    }
+#
+#    END {
+#        for (w in words)
+#             printf("%3d %s\n", words[w], w)
+#    }
+#' |
+#sort -rn
 #--------------------------------------------------------------------- 
