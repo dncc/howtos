b8dac8e2ce8ac36180fdb503b13fe14bd3ce8981
Dragan Cvetinovic <dcvetinovic@gmail.com>
2013-10-09 23:20:14 +0200

    System tweeks, i3, firefox, pop ups etc.

--- system.sh
+++ system.sh
@@ -10,8 +10,8 @@
 # Map caps lock to ctrl
 # --------------------------------------------------------------
 #  System Tools => System Settings => Search for
-# * Gnome: keyboard/regional settings => Edit keyboard/regional settings 
-# => Edit Layout options => caps lock as control (also there is startup 
+# * Gnome: keyboard/regional settings => Edit keyboard/regional settings
+# => Edit Layout options => caps lock as control (also there is startup
 # script in workspace/scripts that does the same thing.
 # * WMII: in .zshrc 'source /usr/local/share/keymappings/caps_to_ctrl.sh'
 
@@ -36,3 +36,72 @@
 # ----------------------------------------------
 # ctrl+alt: edit /etc/default/keyboard:
 	$ XKBOPTIONS="grp:ctrl_alt_toggle"
+
+# Install sshd server and clients
+# ----------------------------------------------
+	$ sudo apt-get install openssh-server openssh-clients
+
+# Add new fonts
+# ----------------------------------------------
+# place font files in /usr/share/fonts/truetype
+# then update your font cache.
+	$ sudo fc-cache -fv
+
+# Tools to install
+# ----------------------------------------------
+#	- GVim
+#   - dotfiles
+# 	- chruby
+# 	- ruby, gems
+# 	- wmii
+#   - tmux
+# 	- R
+# 	- octave
+# 	- openssh-server
+#   - font: Envy Code R
+#   - cmus
+#   - latex
+#
+
+# Multiple screen setup w/ i3
+# ----------------------------------------------
+	$ xrandr --output DP2 --auto --left-of LVDS1
+
+# Remove firefox sidebar:
+# ----------------------------------------------
+# create directory chrome on the following path:
+	$ mkdir ~/.mozilla/firefox/<id>.<profile>/chrome
+# in the chrome dir create file userChrome.css
+# with the following content:
+	@namespace url("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"); /* only needed once */
+	#content browser{margin-right:-13px!important;overflow-y:scroll;overflow-x:hidden;}
+# adjust margin so scroll bar is not visible.
+
+# in order to install matplotlib you need to install freetype so:
+wget http://savannah.nongnu.org/download/freetype/freetype-2.4.0.tar.gz
+tar -zxvf freetype-2.4.0.tar.gz
+cd freetype-2.4.0
+./configure && make && sudo make install
+cd ..
+rm freetype-2.4.0.tar.gz
+rm -r freetype-2.4.0
+
+# Tell firefox which user to run on start up
+# -----------------------------------------------------
+# user definitions are in the file # ~/.mozilla/firefox/profiles.ini
+# add the line from bellow to a definition to make it default one
+Default=1
+
+# Disable annoying OpenSSH pop-up window for entering ssh-passphrase
+# -----------------------------------------------------
+# comment everything in /etc/X11/Xsession.d/90x11-common-ssh-agent file
+# this is the file that X server uses to start ssh-agent
+# remove gnome ssh-askpass-gnome program with:
+sudo apt-get remove ssh-askpass-gnome
+
+# disable annoying pinentry pop up (gpg agent)
+# -----------------------------------------------------
+# comment 'use-agent' line in the ~/.gnupg/gpg.conf
+# use-agent
+# then kill all active gpg-agent processes
+
