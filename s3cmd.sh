27c0fcad911d5ed2890c0b647dafbd2fd3173ecc
Dragan Cvetinovic <dcvetinovic@gmail.com>
2014-01-26 20:06:25 +0100

    Remove indentation

--- s3cmd.sh
+++ s3cmd.sh
@@ -1,9 +1,9 @@
 # show number of files in the census1 dir
-	s3cmd ls "s3://job-queries/input/census1/*" | wc -l
+s3cmd ls "s3://job-queries/input/census1/*" | wc -l
 
 # compute total size of the files in census1 dir
-	s3cmd ls "s3://job-queries/input/census1/*" | awk '{ sum+=$3 } END { print sum }'
+s3cmd ls "s3://job-queries/input/census1/*" | awk '{ sum+=$3 } END { print sum }'
 
 # cp from bucket1 to bucket2 with wildcards
-	s3cmd cp "s3://job-queries/input/census1/*" "s3://job-queries/output/census1/*"
+s3cmd cp "s3://job-queries/input/census1/*" "s3://job-queries/output/census1/*"
 
